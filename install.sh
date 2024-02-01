6d9bef8c086afbcb1c8431927c87090a1f20cf7e
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-01 15:01:58 -0700

    adds Green Day - Saviors (2024) Mp3 320kbps

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ rm Movie-Repo.json
 rm PlayStation-Portable-Repo.json
 rm TV-Repo.json
 rm Windows-Apps-Repo.json
+rm Music-Repo.json
 wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/Windows-Apps-Repo.json
 wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/TV-Repo.json
 wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/PlayStation-Portable-Repo.json
 wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/Movie-Repo.json
 wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/EBook-Repo.json
 wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/3DS-Repo.json
+wget https://raw.githubusercontent.com/Licnix/Repo/main/Config/Repos/Music-Repo.json
